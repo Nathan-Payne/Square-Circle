72a9813aa68148ccd8e315b8f7332c5ae2dc535e
Nathan-Payne <paynee89@yahoo.co.uk>
2019-09-18 17:05:42 +0300

    add fields to project schema

--- models/project.js
+++ models/project.js
@@ -3,8 +3,12 @@ const mongoose = require('mogoose');
 var projectSchema = new mongoose.Schema({
     title: String,
     projectType: {type: String, enum: ["Graphic Design", "Photography", "Art"],
-    shortDescription: String,
-    
+    shortDesc: String,
+    englishDesc: String,
+    lithuanianDesc: String,
+    projectDate: { type: Date, default: Date.now },
+    imgUrl: [String], //feed to img inside of grid element
+    location: Number //determine position on page via class added to div element in grid
 });
 
 
