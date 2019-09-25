77b17cdcde232dbb7addc255e41b596e69d3e46d
Nathan-Payne <paynee89@yahoo.co.uk>
2019-09-25 14:32:01 +0300

    moved topnavbar to header.ejs

--- models/project.js
+++ models/project.js
@@ -7,11 +7,14 @@ var projectSchema = new mongoose.Schema({
     englishDesc: String,
     lithuanianDesc: String,
     projectDate: { type: Date, default: Date.now },
-    imgUrl: [String], //feed to img inside of grid element
+    imgUrl: [String], //feed cloudinary url to img inside of grid element
     location: Number
 });
 
 //location: determine position on page via number passed to css variable which gets passed to 
 //the grid-column and grid-row properties of new items.. grid-auto-columns/rows set size of empty grid elements in between
+//OR
+//grid is pre set to 1 column - width fills content space, assign class to set justify and align-self properties
+//based on location parameter in database... use absolute position to adjust
 
 module.exports = mongoose.model("Project", projectSchema); 
