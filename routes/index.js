9ed898e0a292ccfbaaa767316d3c32a224a46f81
Nathan-Payne <paynee89@yahoo.co.uk>
2019-10-08 18:04:26 +0300

    add registration post route for new admin, add dotenv config

--- routes/index.js
+++ routes/index.js
@@ -24,7 +24,25 @@ router.get("/concept", (req, res) => {
 router.get("/register", (req, res) => {
     res.render('register');
 });
-
+//create admin
+router.post("/register", (req, res)=>{
+    let newAdmin = new Admin({
+        username: req.body.username
+    });
+    if(req.body.adminCode === process.env.REGISTRATION_ADMIN_CODE){
+        Admin.register(newAdmin, req.body.password, (err, admin)=>{
+            if(err){
+                console.error(err)
+                return res.redirect("/register")
+            }
+            passport.authenticate("local")(req, res, function(){
+                res.redirect("/");
+            });
+        });
+    } else {
+        res.redirect("/register");
+    };
+});
 
 
 
