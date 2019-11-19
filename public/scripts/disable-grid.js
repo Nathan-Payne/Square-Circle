c56db1fca788b39f355450c8a67cf9d4f82f099f
Nathan-Payne <paynee89@yahoo.co.uk>
2019-11-19 23:35:33 +0000

    moved js code into public/scripts file, updated art + photography.ejs with markup similar to graphic graphic-design.ejs

---
+++ public/scripts/disable-grid.js
@@ -0,0 +1,26 @@
+let thumbnail = document.getElementsByClassName("thumbnail");
+//thumbProjects is array of selected stringified project fields required for thumbnail
+
+var windowSize = window.matchMedia("(max-width: 767px)");
+windowSize.addListener(disableGrid); //attaches listener function on state change to <767px
+disableGrid(windowSize); //call listener funct
+
+function disableGrid(x){
+    if (x.matches) {
+        //gives all images unique id and remove grid positioning when windowSize<767px
+        for (i=0; i<thumbProjects.length; i++){
+            thumbnail[i].setAttribute("id", `image${i + 1}`)
+
+            thumbnail[i].removeAttribute("style");
+            thumbnail[i].removeAttribute("style");
+        };
+    } else {
+        for (i=0; i<thumbProjects.length; i++){
+            thumbnail[i].setAttribute("id", `image${i + 1}`)
+
+            //for each thumbnail set grid positions from their retrieved database values (windowSize>767px)
+            thumbnail[i].style.gridColumnStart = thumbProjects[i].xPosition;
+            thumbnail[i].style.gridRowStart = thumbProjects[i].yPosition;
+        };
+    }
+}
